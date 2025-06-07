9a96086d7bccdb059c2b70db010d4e2e96966380
luckevey <mariia.bzhezynska@student.sps-cl.cz>
2025-06-07 20:09:25 +0200

    Dopracovani

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// ✅ zde je API router
+const apiRoutes = require('./api/api');
+app.use('/api', apiRoutes);
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
