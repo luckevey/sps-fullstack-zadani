f74ba4ab42d6827b46667ac1dfc2a70134f0a2f2
luckevey <mariia.bzhezynska@student.sps-cl.cz>
2025-06-08 21:57:01 +0200

    adding description

--- database/seed.sql
+++ database/seed.sql
@@ -9,8 +9,8 @@ INSERT INTO categories (name) VALUES ('Beletrie');  -- #3
 
 INSERT INTO books (title, author, `year`, category_id, description)
 VALUES
-('1984', 'George Orwell', 1949, 2, ''),
-('Stoletý stařík', 'Jonas Jonasson', 2009, 3, ''),
-('Dějiny Evropy', 'Norman Davies', 1996, 1, ''),
-('Konec dětství', 'Arthur C. Clarke', 1953, 2, ''),
-('Pán prstenů', 'J.R.R. Tolkien', 1954, 3, '');
+('1984', 'George Orwell', 1949, 2, 'Dystopický román o totalitní společnosti, která neustále sleduje své občany.'),
+('Stoletý stařík', 'Jonas Jonasson', 2009, 3, 'Humorný příběh o staříkovi, který utíká z domova důchodců a mimoděk se zaplete do mezinárodní aféry'),
+('Dějiny Evropy', 'Norman Davies', 1996, 1, 'Rozsáhlá a čtivá publikace mapující historii Evropy od starověku po současnost.'),
+('Konec dětství', 'Arthur C. Clarke', 1953, 2, 'Sci-fi klasika o kontaktu lidstva s mimozemskou civilizací a překvapivém vývoji lidstva.'),
+('Pán prstenů', 'J.R.R. Tolkien', 1954, 3, ' Epická fantasy sága o cestě malého hobita, který nese osud světa v jednom prstenu.');
