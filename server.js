630425aec133cbd7ce87bd19d74e54b1d264a52a
luckevey <mariia.bzhezynska@student.sps-cl.cz>
2025-06-08 15:52:40 +0200

    minor fix

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const express = require('express');
-const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 
 // ✅ zde je API router
 const apiRoutes = require('./api/api');
